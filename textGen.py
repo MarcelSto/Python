3665c59546d235a4c1a8a5bb9f17b67204b1983f
Marcel Stobbe <marcel.stobbe98@outlook.de>
2025-09-25 13:02:14 +0200

    push textGen.py

---
+++ textGen.py
@@ -0,0 +1,9 @@
+# Start
+name = "Marcel" 
+country = "Germany"
+age = "27"
+result = ""
+
+result = "Hello my name is " +name+ " and i am " +age+ " years old and i come frome " +country
+print(result)
+# End
