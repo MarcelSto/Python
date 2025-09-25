f2be9cd08e6e3c7cdbddd70775edc2ab662ffd9b
Marcel Stobbe <marcel.stobbe98@outlook.de>
2025-09-25 14:34:24 +0200

    simple adder docs + prog

---
+++ simpleadder.py
@@ -0,0 +1,11 @@
+#Start - Simple Adder 
+
+#Input first Numbers
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+#add Number 1 and Number 2
+result = number1 + number2
+
+#Output result
+print("The result is:", result)
