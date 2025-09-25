2ca29aac228710e6ad63c810bc910a1cf0571a2e
Marcel Stobbe <marcel.stobbe98@outlook.de>
2025-09-25 14:53:08 +0200

    supplement a comment

--- simpleadder.py
+++ simpleadder.py
@@ -10,7 +10,9 @@ result = number1 + number2
 #Output result
 print("The result is:", result)
 
+#END
+
 
 #input(...): Holt die Eingabe vom Benutzer als Text (String).
-#float(...): Wandelt die Eingabe in eine Gleitkommazahl um (funktioniert auch mit Ganzzahlen).
+#float(...): Wandelt die Eingabe in eine Gleitkommazahl um (funktioniert auch mit Ganzzahlen).(Wird ohne Float als verkettung interpretiert z.B. 5+5=55 statt 10)
 #print(...): Gibt das Ergebnis aus.
