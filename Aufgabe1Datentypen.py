b8c22370de51a290c1fa6ad866a4ba1c25d6a10c
marcel <marcel@home.de>
2025-09-30 22:21:16 +0200

    Aufgabe1Datentypen.py

---
+++ Aufgabe1Datentypen.py
@@ -0,0 +1,21 @@
+#Start - Aufgabe 1 Datentypen 
+
+#Aufgabe 1) Deklariere und Initialisiere eine Ganzzahl mit dem Wert 25
+
+a = int(25)
+ 
+
+#Aufgabe 2) Zeichenkette
+
+begrüßung = "Hallo Welt!"
+
+
+#Aufgabe 3) Gleitkommazahl
+
+pi = float(3.14159)
+
+
+#Aufgabe 4) Boolescher Wert 
+
+Feiertag = True
+
