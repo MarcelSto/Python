10b4756dd0057bd45673c822a596a5c04fa541fb
marcel <marcel@home.de>
2025-10-02 10:04:17 +0200

    ageMachine finsh 1

---
+++ ageMachine.py
@@ -0,0 +1,15 @@
+#Start - ageMachine with Function
+
+#Funktion
+def ageMachine():
+    age = int(input("How old r u?: "))
+    if age >= 50:
+        print("You r old!")
+    elif age < 50: 
+        print("You will get old 2!")
+
+
+#Call Function
+ageMachine()
+
+#End
