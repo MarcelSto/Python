c25c8275782cf1a1ccfab6506322ed236d877267
marcel <marcel@home.de>
2025-10-03 10:22:30 +0200

    changed result = None

--- Aufgaben/ezCalc2.py
+++ Aufgaben/ezCalc2.py
@@ -7,7 +7,7 @@ def again():
          calc() 
         elif answer == "no":                             
             return "Goodbye!"
-        else:                       #catch all for invalid input
+        else:                                               #catch all for invalid input
             print("Invalid Input, please try again")
             again()
 
@@ -17,7 +17,7 @@ def calc():
     print("Welcome to Easy Calculator 2.0")
     operation = input("Enter your Operation(e.g. 1+1): ")  #define varable input | define variable input as string (cant convert 1+1 to float) 
 
-    result = None                                 #define variable result as None (to avoid errors) 
+    #result = None                                 #define variable result as None (to avoid errors) 
 
     if "+" in operation:
         numpart = operation.split("+") #split the input at the operator | variable.split() function splits a string into a list where each word is a list item result[0] + result[1]
@@ -31,14 +31,15 @@ def calc():
     elif "/" in operation:
         numpart = operation.split("/")
         if float(numpart[1]) != 0:                        #check if the user is trying to divide by 0
-             result = float(numpart[0]) / float(numpart[1])
+            result = float(numpart[0]) / float(numpart[1])
         else:
-             print("Error: Division by 0")     
+            result = None
+            print("Error: Division by 0")     
     
-    
+
     if result is not None:
-         if result.is_integer():                        #check if the result is an integer
-             result = int(result)                        #convert the float to an integer
+        if result.is_integer():                        #check if the result is an integer
+            result = int(result)                        #convert the float to an integer
     
     print(f"The result is: {result}")                  #print the result
     
