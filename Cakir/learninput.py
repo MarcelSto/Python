4894487597f387b10c350c262aa7fc012cd18ad4
Marcel Stobbe <marcel.stobbe98@outlook.de>
2025-10-21 10:56:52 +0200

    ende input.py

---
+++ Cakir/learninput.py
@@ -0,0 +1,13 @@
+#Start - Input lernen 
+
+###Möglichkeit 1
+print("Bitte gebe irgendein Text ein")
+eingabe = input()
+print(f"Das hier war deine Eingabe {eingabe}")
+
+###Möglichkeit 2
+
+test = input("Gebe bitte dein Passwort ein")
+print(f"Das hier war dein Passwort {test}")
+
+#Ende
