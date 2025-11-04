0cdb8720294fbfc210022fab36ea956444220f09
Marcel Stobbe <marcel.stobbe98@outlook.de>
2025-11-04 10:23:45 +0100

    stern dreieck hinzugefügt

--- Aufgaben/createline.py
+++ Aufgaben/createline.py
@@ -10,7 +10,7 @@ def createLine(length):
         i += 1                          #i ist am anfang der schleife bei 0 und rechnet nach jedem * print +1 damit die schleife eine ende hat 
     print()
 
-createLine(24)
+#createLine(24)
 
 
 def createlines(länge):
@@ -20,8 +20,28 @@ def createlines(länge):
             print("*", end="")
         else:
             print("-", end="")
-        i += 1
+        i += 1                          # i += 1 ist das leiche wie i = i +1
     print()                             #Das leere print sorgt für einen Zeilenumbruch beim aufruf der funktion
 
-createlines(12)
-createlines(9)
+#createlines(12)
+#createlines(9)
+
+
+def stern(zeilen):
+    i = 1
+    while i <= zeilen:
+        print("*" * i)
+        i = i + 1
+
+stern(5)
+
+def sterne(lines):
+    i = 1
+    while i <= lines:
+        if i % 2 == 0:
+            print("-" * i)
+        else:
+            print("*" * i)
+        i += 1
+
+sterne(6)
